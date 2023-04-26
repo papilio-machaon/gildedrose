5c4a928f60d75a1d2438d7248da38c990e71ce65
papilio-machaon <ape03@479-mac-08.sep.cs.ox.ac.uk>
2023-04-26 11:19:21 +0100

    approvaltest/unittest from lab wednesday

--- src/test/java/com/gildedrose/ApprovalTest.java
+++ src/test/java/com/gildedrose/ApprovalTest.java
@@ -4,12 +4,45 @@ import com.oneeyedmen.okeydoke.junit.ApprovalsRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class ApprovalTest {
     @Rule
     public final ApprovalsRule approver = ApprovalsRule.fileSystemRule("src/test/resources/approval");
 
     @Test
-    public void preservesBehaviour() {
-        approver.assertApproved("foo");
+    public void preservesMainsBehaviour() {
+        StringWriter writer = new StringWriter();
+        PrintWriter out = new PrintWriter(writer);
+
+        out.println("Example of running Gilded Rose");
+
+        Item[] items = new Item[] {
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49) };
+
+        GildedRose app = new GildedRose(items);
+
+        out.println("Day 1 ======================");
+        for (Item item : items) {
+            out.println("item = " + item);
+        }
+
+        out.println("Day 2 ======================");
+        app.updateQuality();
+        for (Item item : items) {
+            out.println("item = " + item);
+        }
+
+        out.println("Day 3 ======================");
+        app.updateQuality();
+        for (Item item : items) {
+            out.println("item = " + item);
+        }
+
+        approver.assertApproved(writer.toString());
     }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,7 +7,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GildedRoseTest {
     @Test
-    public void foo() {
-        assertThat("foo", equalTo("foo"));
+    public void GildedRoseNegativeSellIn() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", -15, 0),
+                new Item("Elixir of the Mongoose", -55, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40) };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertThat(items[3].quality, equalTo(43));
+    }
+
+    @Test
+    public void GildedRoseNegativeSellIn2() {
+        Item[] items = new Item[] {
+                new Item("Aged Brie", -15, 0),
+                new Item("Elixir of the Mongoose", -55, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", -35, 40) };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assertThat(items[3].quality, equalTo(0));
     }
 }
